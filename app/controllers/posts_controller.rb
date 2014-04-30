b12b2c17aa7f9b914684bbea28137532fa053546
Alumno <alumno@localhost.localdomain>
2014-04-30 17:09:43 -0400

    Small fix with update post method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,7 +29,7 @@ class PostsController < ApplicationController
 	def update
 		@post = Post.find(params[:id])
 
-		if @post.update(params[:post])
+		if @post.update(params[:post].permit(:title, :text))
 			redirect_to @post
 		else
 			render 'edit'
